00eebdcc830727b0cca0ba27113cbd3ded44bf0d
“[Ryan <[ryanmiller12@bellsouth.net]>
2018-07-17 10:39:20 -0400

    created message via React coding

---
+++ index.js
@@ -0,0 +1,3 @@
+//React Content
+let reactMsg = React.createElement("h1", null, "This message was given to you by React!");
+ReactDOM.render(reactMsg, document.querySelector("[targetDataReact]git"));
